97fea5263e0ff360ebdbc652a23d17bf156bb9bb
Kate Catlin <katecatlin@KateCatlin.local>
2023-02-16 19:56:27 -0600

    Adding 2nd commitment and a delay between posts.

--- scheduled.py
+++ scheduled.py
@@ -7,18 +7,34 @@ from slack_sdk.errors import SlackApiError
 
 logging.basicConfig(level=logging.DEBUG)
 
-def sendMessage(slack_client, msg):
+def sendFirstCommitment(slack_client, msg1):
   # make the POST request through the python slack client
   
   # check if the request was a success
   try:
     slack_client.chat_postMessage(
       channel='#test',
-      text=msg
+      text=msg1
     )#.get()
   except SlackApiError as e:
     logging.error('Request to Slack API Failed: {}.'.format(e.response.status_code))
     logging.error(e.response)
+
+
+def sendSecondCommitment(slack_client, msg2):
+  # make the POST request through the python slack client
+  
+  # check if the request was a success
+  try:
+    slack_client.chat_postMessage(
+      channel='#test',
+      text=msg2
+    )#.get()
+  except SlackApiError as e:
+    logging.error('Request to Slack API Failed: {}.'.format(e.response.status_code))
+    logging.error(e.response)
+
+
 
 if __name__ == "__main__":
   SLACK_BOT_TOKEN = os.environ['REMINDER_SLACK_TOKEN']
@@ -26,11 +42,20 @@ if __name__ == "__main__":
   slack_client = WebClient(SLACK_BOT_TOKEN)
   logging.debug("authorized slack client")
 
-  msg = "*Daily Conscious Leadership commitment reminder: Responsibility*. I commit to taking full responsibility for the circumstances of my life, and my physical, emotional, mental and spiritual wellbeing. I commit to support others to take full responsibility for their lives."
-  schedule.every(15).seconds.do(lambda: sendMessage(slack_client, msg))
+  msg1 = "*Daily Conscious Leadership commitment reminder: Responsibility.* I commit to taking full responsibility for the circumstances of my life, and my physical, emotional, mental and spiritual wellbeing. I commit to support others to take full responsibility for their lives."
+  msg2 = "*Daily Conscious Leadership commitment reminder: Curiosity.* I commit to growing in self-awareness. I commit to regarding every interaction as an opportunity to learn. I commit to curiosity as a path to rapid learning."
+
+  schedule.every(5).seconds.do(lambda: sendSecondCommitment(slack_client, msg1))
+  time.sleep(3)
+  schedule.every(5).seconds.do(lambda: sendSecondCommitment(slack_client, msg2))
+
 
   logging.info("entering loop")
 
   while True:
     schedule.run_pending()
     time.sleep(1) # sleep for 1 second between checks
+
+
+
+
